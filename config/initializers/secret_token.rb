39e367f41618937d8cdad5a59e0c06db43dda599
teabait <hello.teaho@gmail.com>
2014-01-07 14:25:58 -0500

    first commit wheee

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GA::Application.config.secret_key_base = '6095ccbf442d1bf5160b7bad14bb84adcf30c512305a42b040cb3a7a92ae1a8edcefb655539c6099072def1738d96ac5732a952ede1dd55bd2b1828927ab070f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GA::Application.config.session_store :cookie_store, key: '_GA_session'
