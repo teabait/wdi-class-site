3f45c0178cda47f6d0b3c63f91757c084b417dd8
teabait <hello.teaho@gmail.com>
2014-01-09 14:49:37 -0500

    student profile layout

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -27,11 +27,13 @@ function studentCall(student){
   var studentInfo = student;
 
   var list = $("#student-info");
-  var github= $("<li>").html($("<a>").text("github").attr("href", "http://github.com/" + student.github));
-  var email = $("<li>").html($("<a>").text("email").attr("href", "mailto:" + student.email));
-  var website = $("<li>").html($("<a>").text("website").attr("href", student.website));
+  var name = $("<p>").text(student.name).attr("class", "name");
+  var github= $("<li>").html($("<a>").text("github").attr({href: "http://github.com/" + student.github, class: "contact"}));
+  var email = $("<li>").html($("<a>").text("email").attr({href: "mailto:" + student.email, class: "contact"}));
+  var website = $("<li>").html($("<a>").text("website").attr({href: student.website, class: "contact"}));
   var picture = $("<img>").attr({src: "/assets/" + student.picture, class: "photo"});
-  list.empty().append(github).append(email).append(website).append(picture);
+  var blurb = $("<p>").text(student.blurb).attr("class", "blurb");
+  list.empty().append(picture).append(name).append(blurb).append(github).append(email).append(website);
 
 
 }
